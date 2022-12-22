6aca864f7dd5911e26d0017a372b1c9e641c48b5
Salavat <malikov.salavat702@gmail.com>
2022-12-22 10:34:00 +0300

    1

---
+++ Example_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+// число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Веедите число");
+string num = (Console.ReadLine());
+
+int SumNum()
+{
+    int sum = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        sum = sum + (num[i] - '0');
+    }
+    return sum;
+}
+Console.WriteLine(SumNum());
